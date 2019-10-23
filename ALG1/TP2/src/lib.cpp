5ae84af302ca425a2781475e8768da81d49e7ca5
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2019-10-22 21:36:41 -0300

    Using _islands on pd instead of 2 vectors

--- ALG1/TP2/src/lib.cpp
+++ ALG1/TP2/src/lib.cpp
@@ -8,16 +8,13 @@ Lib::~Lib() = default;
 
 
 void Lib::set_islands(std::vector<std::tuple<float, int, int>> island){
-  _islands = island;
+  _islands = island; //Cash that I have
 }
 
 void Lib::set_greedy(){
-  int temp = 0, i = 0;
+  int i = 0, temp = _N;
 
  std::stable_sort(_islands.begin(), _islands.end());
-
-  temp = _N; //Cash that I have
-
 
   while(temp > 0 && i < _islands.size()){
     while( std::get<1>(_islands[i]) > temp && i < _islands.size()){
@@ -28,7 +25,6 @@ void Lib::set_greedy(){
       break;
     // The money that i have - cost of the island
     temp -= std::get<1>(_islands[i]);
-
     _days++;
     _total_points += std::get<2>(_islands[i]);
 
@@ -36,24 +32,19 @@ void Lib::set_greedy(){
 }
 
 void Lib::set_dynamic_programming(int N, int M){
-  std::vector<int> Cost, Points;
   std::vector<std::vector<int>> _matrix = std::vector<std::vector<int>>(M+1);
   for(int i = 0; i < M+1; i++)
     _matrix[i] = std::vector<int>(N+1);
-
-  for(int i = 0, k = 0; i < _islands.size(); i++, k++){
-    Cost.push_back(std::get<1>(_islands[i]));
-    Points.push_back(std::get<2>(_islands[i]));
-  }
 
   for(int i = 0; i <= M; i++){
     for(int j = 0; j <= N; j++){
       if(i == 0 || j == 0)
         _matrix[i][j] = 0; 
-      else if(Cost[i-1] > j)
+      else if(std::get<1>(_islands[i-1]) > j)
         _matrix[i][j] = _matrix[i-1][j];
       else
-        _matrix[i][j] = std::max(_matrix[i-1][j], Points[i-1] + _matrix[i-1][j - Cost[i-1]]);
+        _matrix[i][j] = std::max(_matrix[i-1][j], std::get<2>(_islands[i-1]) +
+        _matrix[i-1][j - std::get<1>(_islands[i-1])]);
     }
   }
 
@@ -64,7 +55,7 @@ void Lib::set_dynamic_programming(int N, int M){
   while(i >= 1 && j >= 0){
     if(_matrix[i][j] > _matrix[i-1][j]) {
       _days_pd++;
-      j = j - Cost[i - 1];
+      j = j - std::get<1>(_islands[i - 1]);
     }
     i--;
   }
