a0a9fa412f61d802dc4ace097fd68f80ec5682b8
Roberto Rosmaninho <robertogrosmaninho@outlook.com>
2019-11-19 01:51:02 -0300

    mapping correctly matrix to graph

--- ALG1/TP_3/src/include/MatrixToGraph.h
+++ ALG1/TP_3/src/include/MatrixToGraph.h
@@ -8,27 +8,27 @@
 
 #include <utility>
 #include <vector>
+#include <map>
 
 class Node{
 private:
-  int _id, _number, _line, _column;
+  int _number, _line, _column;
 public:
-  Node(int id, int number, int line, int column) : _id(id), _number(number),
-                                                   _line(line), _column(column) {}
+  Node( int number, int line, int column) : _number(number), _line(line),
+                                            _column(column) {}
   ~Node() = default;
 
-  int get_id(){ return _id; }
+  /*int get_id(){ return _id; }*/
   int get_number(){ return _number; }
   int get_line(){ return _line; }
   int get_column(){ return _column; }
 };
 
-
 class MatrixToGraph {
 private:
   int block_column, block_line;
-  std::vector<std::pair<int, int>> blocks;
-  std::vector<Node>* graph;
+  std::vector<Node> nodes;
+  std::vector<Node>* _graph;
 
 public:
   MatrixToGraph(int V, int I, int J, std::vector<int>* matrix);
--- ALG1/TP_3/src/lib/MatrixToGraph.cpp
+++ ALG1/TP_3/src/lib/MatrixToGraph.cpp
@@ -8,52 +8,62 @@
 MatrixToGraph::MatrixToGraph(int V, int I, int J,
                               std::vector<int> *matrix) :
                               block_line(J), block_column(I) {
-  this->graph = new std::vector<Node>[V*V];
+  this->_graph = new std::vector<Node>[V * V];
 
+  int h = 0, i = 0, j = 0, k = 0;
+  int e = 0, f = 0, x = 0;
+  int size = block_line * block_column;
 
-  int h = 0, i = 0 , j = 0, k = 0;
+  //Itera sobre as linhas em blocos
+  for (; h < V; h += block_line, e += (block_line), f = 0) {
+    //Itera sobre as colunas em blocos
+    for (i = 0; i < V; i += block_column, f += (block_column)) {
+      //Itera sobre as linhas dos blocos
+      for (j = 0; j < block_line; j++) {
+        //Itera sobre as colunas dos blocos
+        for (k = 0; k < block_column; k++, x++) {
+          //Adiciona todos os itens dobloco em uma lista
+          nodes.emplace_back(matrix[j + e][k + f], j + e, k +f);
+          //Cria um nó no grafo com o número atual
+          _graph[x].emplace_back(matrix[j + e][k + f], j + e, k +f);
+        }
+      }
+      x -= size; //Retorna o iterador para a posicão anterior à adição dos
+      // numeros do bloco atual para passar por cada item novamente
 
-  //Set the first nodes
-  for(i = 0; i < V; i++) {
-    for (j = 0; j < V; j++) {
-      graph[k].emplace_back(k, matrix[i][j], i, j);
-      k++;
+      //Itera sobre cada item para que loop inferior adcione cada item da
+      // lista ao nó atual no grafo
+      for(int p = 0; p < nodes.size(); p++, x++) {
+        for (auto node : nodes) {
+          if(!(node.get_line() == _graph[x][0].get_line() &&
+              node.get_column() == _graph[x][0].get_column())){
+            _graph[x].push_back(node);
+          }
+        }
+      }
+      nodes.clear(); //Reinicia a lista
     }
   }
 
-
-  k=0;
-  int line_test = 0, column_test = 0;
-  int column = 0, line = block_line;
-  //Set the blocks
-  h = 0, i = 0, j = 0;
-  //Column
-  for(i = block_column - 1, j = block_line - 1; i < V; i+=block_column){
-    blocks.emplace_back(i, j);
-    for(j+=block_line; j < V; j+=block_line)
-      blocks.emplace_back(i, j);
-  }
+  //Fill the nodes with the lines
+  for(int a = 0; a < V*V; a++)
+    for(int b = _graph[a][0].get_line(), c = 0; c < V; c++)
+      if(!(_graph[a][0].get_line() == b && _graph[a][0].get_column() == c))
+        _graph[a].emplace_back(matrix[b][c], b, c);
 
 
-  /*k = 0;
-  for(int i = 0; i < V; i++){
-    std::cout << "i(" << i << ") -> ";
-    for(int j = 0; j < V; j++){
-      if(k == j)
-        continue;
-      std::cout << matrix[j][i] << " ";
-      graph[k].push_back(matrix[j][i]);
-      k++;
-    }
-    std::cout << "\n";
-    k = 0;
-  }*/
+  //Fill the nodes with columns
+  for(int a = 0; a < V*V; a++)
+    for (int b = 0, c = _graph[a][0].get_column(); b < V; b++)
+      if (!(_graph[a][0].get_line() == b && _graph[a][0].get_column() == c))
+        _graph[a].emplace_back(matrix[b][c], b, c);
+
 
   std::cout << "===================================\n";
   for(int i = 0; i < V*V; i++){
-    std::cout << "i(" << i << ", " << graph[i][0].get_number() << ") -> ";
-    for(long j = 0; j < graph[i].size(); j++) {
-      std::cout << graph[i][j].get_number() << " ";
+    std::cout << "i(" << i << ", " << _graph[i][0].get_number() << ") -> ";
+    for(long j = 1; j < _graph[i].size(); j++) {
+      std::cout << _graph[i][j].get_number() << " ";
     }
     std::cout << "\n";
   }
